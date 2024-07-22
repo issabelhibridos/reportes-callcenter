9d218f09c54f4ec1f15fd49243077093bc9c5a3d
issabelhibridos <issabelhibridos@gmail.com>
2024-07-22 09:20:10 -0500

    Update useTitle.js

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export const useTitle = (title) => {
     useEffect(() => {
-        document.title = `${title} / #Beercat`;
+        document.title = `${title} / #HibridosTI`;
     })
     return null
 }
