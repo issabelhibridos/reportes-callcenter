e6090a7e2094ec42e2859725cff780d098e3eeb6
Issabel Hibridos <issabelhibridos@gmail.com>
2024-08-20 11:29:16 -0500

    Pbxapi server connection

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -2,12 +2,12 @@ export async function reportAuth() {
 
     const requestOptions = {
         method: "GET",
-        headers: { "content-Type": "application/json", Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjE1MzYzNjMsImV4cCI6MTcyMTUzOTk2MywiZGF0YSI6eyJuYW1lIjoiYWRtaW4ifX0.I5jOjWgUsyFRwDXvmZBkgR7JkUCmQVFNKJ8rlF3FJD8` },
+        headers: { "content-Type": "application/json", Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjQxNzAzODEsImV4cCI6MTcyNDE3Mzk4MSwiZGF0YSI6eyJuYW1lIjoiYWRtaW4ifX0.s6ZkT6rU333Q70zLYeEvocbT1Qc_YQTindzETgJEBms` },
         // method: "POST",
         // body: { user: "admin", password: "N0m3l4s3" },
     }
 
-    const response = await fetch(`${process.env.REACT_APP_ISSABEL_HOST}admints`, requestOptions);
+    const response = await fetch(`${process.env.REACT_APP_ISSABEL_HOST}trunks`, requestOptions);
 
     if (!response.ok) {
         const errorMessage = { message: response.statusText, status: response.status };
